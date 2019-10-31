1c93c95a67cdc90e4f3b41b3843033416d16ea64
Ram <ramdevukd@gmail.com>
2019-10-31 16:05:09 +0530

    added form validation in SignUp form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,26 +3,76 @@ import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 class SignUp extends Component {
-  state = {};
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    nameErr: null,
+    emailErr: null,
+    passErr: null
+  };
+
+  onInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = () => {
+    if (!validator.isLength(this.state.name, { min: 3 })) {
+      this.setState({ nameErr: "Name should be atleast 3 characters long :(" });
+      return;
+    } else {
+      this.setState({ namelErr: null });
+    }
+
+    if (!validator.isEmail(this.state.email)) {
+      this.setState({ emailErr: "Please enter a valid email :(" });
+      return;
+    } else {
+      this.setState({ emailErr: null });
+    }
+
+    if (!validator.isLength(this.state.password, { min: 6 })) {
+      this.setState({
+        passErr: "Password must be atleast 6 characters long :("
+      });
+      return;
+    } else {
+      this.setState({ passErr: null });
+    }
+  };
   render() {
     return (
       <div className="main-signup">
         <div className="form-signup">
           <div className="form-field-signup">
             <p>Name</p>
-            <input className="text-field-signup" name="name" type="text" />
+            <input
+              onInput={this.onSubmit}
+              className="text-field-signup"
+              name="name"
+              type="text"
+            />
+            <span>{this.state.nameErr}</span>
           </div>
           <div className="form-field-signup">
             <p>Email</p>
-            <input className="text-field-signup" name="email" type="text" />
+            <input
+              onInput={this.onSubmit}
+              className="text-field-signup"
+              name="email"
+              type="text"
+            />
+            <span>{this.state.emailErr}</span>
           </div>
           <div className="form-field-signup">
             <p>Password</p>
             <input
+              onInput={this.onSubmit}
               className="text-field-signup"
               name="password"
               type="password"
             />
+            <span>{this.state.passErr}</span>
           </div>
           <p className="submit-signup">Sign Up</p>
 
