6f672ffeb5cc862f540a95bac08a443aaabc1aa7
Ram <ramdevukd@gmail.com>
2019-11-01 18:19:07 +0530

    added BasketReducer and BasketAction files with logic pending

--- src/redux/combineReducers.js
+++ src/redux/combineReducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import UserReducer from "./User/UserReducer";
 import ProductReducer from "./Product/ProductReducer";
+import BasketReducer from "./Basket/BasketReducer";
 
 export default combineReducers({
+  UserReducer,
   ProductReducer,
-  UserReducer
+  BasketReducer
 });
